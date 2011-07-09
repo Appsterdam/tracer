60d99528f7379a80cbc1273586f320eeb90554a9
Matteo Manferdini <matteo.manferdini@gmail.com>
2011-07-09 10:52:56 +0200

    Created XCode project

---
+++ The Race App/The_Race_AppAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  The_Race_AppAppDelegate.h
+//  The Race App
+//
+//  Created by Matteo Manferdini on 09/07/11.
+//  Copyright 2011 Pawn Company Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface The_Race_AppAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
---
+++ The Race App/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  The Race App
+//
+//  Created by Matteo Manferdini on 09/07/11.
+//  Copyright 2011 Pawn Company Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+	int retVal = UIApplicationMain(argc, argv, nil, nil);
+	[pool release];
+	return retVal;
+}
