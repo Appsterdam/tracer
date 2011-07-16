fca4b20bea672384a7ca9ee10444beb56e48f35c
Klaas Speller <klaasspeller@gmail.com>
2011-07-16 18:23:22 +0200

    Work on api

--- app/controllers/tracks.rb
+++ app/controllers/tracks.rb
@@ -26,4 +26,33 @@ TheRaceApp.controllers :tracks do
       }.to_json
     end
   end
+
+  post :start, :map => ":id/start", :provides => :json do
+    track = Track.get(params[:id])
+    race = track.start_race(params[:username])
+    {
+      :ok => true,
+      :data => {
+        :race => race.id,
+        :stop => url(:tracks, :stop, :id => track.id, :race => race.id, :format => content_type),
+        :username => race.username,
+        :started => race.started
+      }
+    }.to_json
+  end
+
+  post :stop, :map => ":id/races/:race/stop", :provides => :json do
+    track = Track.get(params[:id])
+    race = track.races.first(:id => params[:race])
+    race.stop(params[:time])
+
+    {
+      :ok => true,
+      :data => {
+        :time => race.duration,
+        :won => (track.best_race == race),
+        :winner => track.best_race.username
+      }
+    }.to_json
+  end
 end
--- spec/app/controllers/tracks_controller_spec.rb
+++ spec/app/controllers/tracks_controller_spec.rb
@@ -2,6 +2,9 @@ require 'spec_helper'
 
 describe "TracksController" do
   let(:json) { JSON.parse(last_response.body) }
+  before do
+    DataMapper.auto_migrate!
+  end
 
   before do
     get "/"
@@ -9,7 +12,6 @@ describe "TracksController" do
 
   describe "GET /tracks.json" do
     before do
-      DataMapper.auto_migrate!
       3.times { Track.gen }
       get TheRaceApp.url(:tracks, :index)
     end
@@ -41,7 +43,6 @@ describe "TracksController" do
     let(:track) { Track.gen }
     
     before do
-      DataMapper.auto_migrate!
       track
       get TheRaceApp.url(:tracks, :show, :id => track.id)
     end
@@ -67,6 +68,86 @@ describe "TracksController" do
 
       it 'stores the name of the track' do
         data["name"].should == track.name
+      end
+    end
+  end
+
+  describe "POST /tracks/1/start.json" do
+    let(:track) { Track.gen }
+
+    before do
+      post TheRaceApp.url(:tracks, :start, :id => track.id), { :username => "emma" }
+    end
+
+    it 'responds with json' do
+      last_response.content_type.should =~ %r{application/json}
+    end
+
+    it 'creates an new race' do
+      expect do
+        post TheRaceApp.url(:tracks, :start, :id => track.id), { :username => "emma" }
+      end.to change(track.races, :count).by(1)
+    end
+
+    describe "json response" do
+      it 'sets "ok" to true' do
+        json["ok"].should == true
+      end
+
+      it 'stores the id of the race under the "data.race" key' do
+        json["data"]["race"].should == track.races.last.id
+      end
+
+      it 'stores the stop uri of the race under the "data.stop" key' do
+        json["data"]["stop"].should == TheRaceApp.url(:tracks, :stop, :id => track.id, :race => json["data"]["race"], :format => :json)
+      end
+
+      it 'stores the username under the "data.username" key' do
+        json["data"]["username"].should == "emma"
+      end
+      
+      it 'stores the start time under the "data.started" key' do
+        json["data"]["started"].should_not be_nil
+      end
+    end
+  end
+
+  describe "POST /tracks/1/races/1/stop.json" do
+    let(:track) { Track.gen }
+    let(:race) { track.start_race("emma") }
+
+    before do
+      post TheRaceApp.url(:tracks, :stop, :id => track.id, :race => race.id), { :time => 600 }
+    end
+
+    it 'responds 200 ok' do
+      last_response.should be_ok
+    end
+
+    it 'responds with json' do
+      last_response.content_type.should =~ %r{application/json}
+    end
+
+    it 'stops the race' do
+      race.reload
+      race.progress?.should == false
+    end
+
+    describe "json data" do
+      it 'sets the "ok" key to true' do
+        json["ok"].should == true
+      end
+
+      it 'stores the time under the "data.time" key' do
+        json["data"]["time"].should == 600
+      end
+
+      it 'set the "data.won" key to true if the race was won by the user' do
+        json["data"]["won"].should == true
+      end
+
+      it 'sets the "data.winner" key to the username of the user with the best time' do
+        json["data"]["winner"].should == track.best_race.username
       end
     end
   end
