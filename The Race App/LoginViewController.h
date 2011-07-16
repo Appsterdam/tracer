40f6c7d6e29fdfc033148049fadfd61f0fcc75bb
Sergey A. Novitsky <serieux_chat@hotmail.com>
2011-07-16 12:02:32 +0200

    Added LoginViewController

---
+++ The Race App/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  The Race App
+//
+//  Created by Sergey Novitsky on 7/16/11.
+//  Copyright 2011 Pawn Company Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
